95c7f85bc4d0265ab6560682292f3b21165f5629
Angela J. Holden <angelajholden@gmail.com>
2020-05-24 08:50:22 -0500

    working on structure

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-
-	<?php include_once('partials/head.php'); ?>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Angela J. Holden | Designer &amp; Developer</title>
+		<meta name="description" content="Frontend developer, user interface (UI) and user experience (UX) designer.">
+		<link rel='dns-prefetch' href='//fonts.googleapis.com'>
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600" rel="stylesheet">
+		<link rel="stylesheet" href="/style.css">
+		<link rel="shortcut icon" href="/favicon.png">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script type="text/javascript" src="/js/main.js"></script>
+	</head>
 
 	<body class="home">
 
@@ -26,13 +37,13 @@
 
 			<section class="content">
 				<article class="yellow_box">
-					<h2 class="sub_head">Well, hello there!</h2>
+					<h2 class="sub_head">Hello there</h2>
 					<p>My name is Angela and I’m a UI/UX designer and frontend developer. I love my dog, the piano and coding. In that order. Rounding out my list are cheap wine, coffee and books.</p>
 					<button><a class="button" href="#">More About Me</a></button>
 				</article>
 
 				<figure class="img_box">
-					<img src="images/angelajholden-piano-softlight.jpg" alt="Close up of piano keyboard with softlight.">
+					<img src="/images/angelajholden-piano-softlight.jpg" alt="">
 				</figure>
 			</section>
 
