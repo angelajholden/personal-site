7fde0cece7c36656e529799d25f18beb6aca71b1
Angela J. Holden <hold4477@stthomas.edu>
2020-06-12 08:48:02 -0500

    cleaning up nav urls

--- partials/base.php
+++ partials/base.php
@@ -1,8 +1,3 @@
-<!-- development on MAMP free (localhost) -->
 <!-- <base href="http://localhost/personal-site/"> -->
-
-<!-- development on MAMP Pro -->
 <!-- <base href="http://angelajholden/"> -->
-
-<!-- make sure this is the base deployed to Heroku -->
-<base href="http://angelajholden.herokuapp.com/">
+<!-- <base href="http://angelajholden.herokuapp.com/"> -->
