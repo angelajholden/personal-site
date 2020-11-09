bf1d23510502b7642af7d179ad7eb664d4df5081
Angela J. Holden <hold4477@stthomas.edu>
2020-11-09 14:29:32 -0600

    animate the circle

--- partials/nav.php
+++ partials/nav.php
@@ -2,20 +2,20 @@
 <a href="#" aria-label="Open Menu" id="open" class="menu-open"><span class="menu-icon">≡</span></a>
 <nav id="nav" class="nav-primary">
 	<ul id="nav__items">
-		<li class="nav_item"><a href="/">Home</a></li>
-		<li class="nav_item has-children"><a href="/about/">About</a>
+		<li class="nav_item"><a href="./">Home</a></li>
+		<li class="nav_item has-children"><a href="./about/">About</a>
 			<ul class="sub_menu">
 				<li class="nav_item"><a href="#">Résumé</a></li>
 			</ul>
 		</li>
-		<li class="nav_item has-children"><a href="/skills/">Skills</a>
+		<li class="nav_item has-children"><a href="./skills/">Skills</a>
 			<ul class="sub_menu">
-				<li class="nav_item"><a href="/skills/ui-ux/">UI/UX</a></li>
-				<li class="nav_item"><a href="/skills/frontend/">Frontend</a></li>
-				<li class="nav_item"><a href="/skills/accessibility/">Accessibility</a></li>
+				<li class="nav_item"><a href="./skills/ui-ux/">UI/UX</a></li>
+				<li class="nav_item"><a href="./skills/frontend/">Frontend</a></li>
+				<li class="nav_item"><a href="./skills/accessibility/">Accessibility</a></li>
 			</ul>
 		</li>
-		<li class="nav_item"><a href="/portfolio/">Portfolio</a></li>
+		<li class="nav_item"><a href="./portfolio/">Portfolio</a></li>
 		<li><a href="mailto:info@angelajholden.com">Contact</a></li>
 	</ul>
 	<a href="#" aria-label="Close Menu" id="close" class="menu-close"><span class="menu-icon">×</span></a>
