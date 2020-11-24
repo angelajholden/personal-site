1561b2be076630d404f3eab46d400391c272f8d3
Angela J. Holden <angelajholden@gmail.com>
2020-11-24 15:10:48 -0600

    formatting, contact form, resume

--- partials/nav.php
+++ partials/nav.php
@@ -1,11 +1,11 @@
-<a href="#skip-nav">Skip Navigation</a>
+<a class="screen-reader-text" href="#skip-nav">Skip Navigation</a>
 <a href="#" aria-label="Open Menu" id="open" class="menu-open"><span class="menu-icon">≡</span></a>
 <nav id="nav" class="nav-primary">
 	<ul id="nav__items">
 		<li class="nav_item"><a href="./">Home</a></li>
 		<li class="nav_item has-children"><a href="./about/">About</a>
 			<ul class="sub_menu">
-				<li class="nav_item"><a href="#">Résumé</a></li>
+				<li class="nav_item"><a href="./about/Angela_Holden_Resume.pdf" target="_blank">Résumé</a></li>
 			</ul>
 		</li>
 		<li class="nav_item has-children"><a href="./skills/">Skills</a>
@@ -16,7 +16,7 @@
 			</ul>
 		</li>
 		<li class="nav_item"><a href="./portfolio/">Portfolio</a></li>
-		<li><a href="mailto:info@angelajholden.com">Contact</a></li>
+		<li class="nav_item"><a href="./contact/">Contact</a></li>
 	</ul>
 	<a href="#" aria-label="Close Menu" id="close" class="menu-close"><span class="menu-icon">×</span></a>
 </nav>
